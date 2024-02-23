1c2a93c1a052712d1cff4ee46e0ffc4c21ce26fa
ppagaduan <129784067+ppagaduan@users.noreply.github.com>
2024-02-23 11:50:37 -0800

    issues with scrollyteller import (currently commented out)

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,7 @@
 <script>
-  // Write your JS here, or import other files
+  import * as d3 from 'd3';
+  // import ScrollyTeller from './ScrollyTeller.svelte';
+  import { onMount } from "svelte";
 </script>
 
 <main>
@@ -9,7 +11,39 @@
 </main>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap');
+
+  :root {
+    --color-bg: #000000;
+    --color-outline: #c2c2c2;
+    --color-shadow: hsl(0, 0%, 0%, 0.1);
+    --color-text: #000000;
+    --color-bg-1: hsla(0, 0%, 0%, 0.2);
+    --color-shadow-1: hsl(0, 0%, 96%);
+  }
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    text-align: center;
+    font-family: 'Nunito', sans-serif;
+    font-weight: 300;
+    line-height: 2;
+    font-size: 24px;
+    color: var(--color-text);
+  }
+
   .title{
-    font-size: 700;
+    font-size: 2em;
+    font-weight: 300;
+    line-height: 2;
+    background-color: #F4F0E8;
+    color: #D6221D
   }
 </style>
--- src/components/scrollyteller.svelte
+++ src/components/scrollyteller.svelte
@@ -1,74 +1,38 @@
 <script>
-    import Scroller from "@sveltejs/svelte-scroller";
-    import Map from "./Map.svelte";
-    import { geoMercator } from "d3-geo";
-    import Graph from "./Graph.svelte";
+    // import Scroller from "@sveltejs/svelte-scroller";
   
-    let count, index, offset, progress;
-    let width, height;
-  
-    let geoJsonToFit = {
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [1, 0],
-          },
-        },
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [0, 1],
-          },
-        },
-      ],
-    };
-  
-    $: projection = geoMercator().fitSize([width, height], geoJsonToFit);
+    // let count, index, offset, progress;
   </script>
   
-  <Scroller
-    top={0.0}
-    bottom={1}
-    threshold={0.5}
-    bind:count
-    bind:index
-    bind:offset
-    bind:progress
-  >
-    <div class="background" 
-         slot="background"
-         bind:clientWidth={width}
-         bind:clientHeight={height}
-    >
-  
-      <Map bind:geoJsonToFit {index} />
-      <Graph {index} {width} {height} {projection} />
-  
-      <div class="progress-bars">
-        <p>current section: <strong>{index + 1}/{count}</strong></p>
-        <progress value={count ? (index + 1) / count : 0} />
-  
-        <p>offset in current section</p>
-        <progress value={offset || 0} />
-  
-        <p>total progress</p>
-        <progress value={progress || 0} />
-      </div>
+  <!-- <Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div class="background" slot="background">
+
+    <div class="progress-bars">
+      <p>current section: <strong>{index + 1}/{count}</strong></p>
+      <progress value={count ? (index + 1) / count : 0} />
+
+      <p>offset in current section</p>
+      <progress value={offset || 0} />
+
+      <p>total progress</p>
+      <progress value={progress || 0} />
     </div>
-  
-    <div class="foreground" slot="foreground">
-      <section>This is the first section.</section>
-      <section>This is the second section.</section>
-      <section>This is the third section.</section>
-      <section>This is the fourth section.</section>
-      <section>This is the fifth section.</section>
-      <section>This is the sixth section.</section>
-    </div>
-  </Scroller>
+  </div>
+
+  <div class="foreground" slot="foreground">
+    <section>This is the first section.</section>
+    <section>This is the second section.</section>
+    <section>This is the third section.</section>
+  </div>
+</Scroller> -->
   
   <style>
     .background {
