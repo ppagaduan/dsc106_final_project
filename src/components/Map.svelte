dfcb030dcf79221eae34332e71bb495edf712b0f
ppagaduan <129784067+ppagaduan@users.noreply.github.com>
2024-02-27 13:54:47 -0800

    2/27/24 1:54pm

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -3,10 +3,26 @@
 	const map = new mapboxgl.Map({
 		container: "map",
 		style: "mapbox://styles/mapbox/light-v11", 
-		center: [-71.0942, 42.3601], 
+		center: [0, 0], 
 		zoom: 13, // starting zoom level
 		minZoom: 12,
 		maxZoom: 15,
 	});
+    map.on("load", () => {
+		map.addSource("world_map", {
+			type: "geojson",
+			data: "https://raw.githubusercontent.com/ppagaduan/dsc106_final_project/main/static/world_map.json",
+		});
+		map.addLayer({
+			id: "world_map",
+			type: "line",
+			source: "world_map",
+			paint: {
+				"line-color": "#BEE5B0",
+				"line-width": 3,
+			},
+		});
+	});
+
 </script>
 
